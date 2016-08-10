120b8e802a7a907ec620ffffd766dfa44de7c84b
Eugene <Eugene@EUGENE-LAPTOP>
2016-08-11 00:07:19 +0300

    Add Prop

--- WebApplication1/WebApplication1/Models/Employee.cs
+++ WebApplication1/WebApplication1/Models/Employee.cs
@@ -9,6 +9,7 @@ namespace WebApplication1.Models
     public class Employee
     {
         [Key]
+        public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Salary { get; set; }
